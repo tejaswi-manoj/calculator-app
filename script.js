341dc15524bb1a10ffe48a6ca82e61a34ab76ab9
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-07-05 00:42:52 -0400

    Create basic calculator functions

---
+++ script.js
@@ -0,0 +1,21 @@
+let a; // first variable
+let o; // operator
+let b; //second variable
+
+function add(a,b){
+    return (a+b);
+}
+
+function multiply(a,b){
+    return (a*b);
+}
+
+function divide(a, b) {
+    return Math.ceil((a / b) * 100) / 100;
+}
+
+function subtract(a,b){
+    return (a-b);
+}
+
+console.log(subtract(0,3));
