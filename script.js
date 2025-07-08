d3a3e8c4be6cc39e0f6ab9f8f464f0bc83fa0c78
Tejaswi Manoj <tejaswimanoj@gmail.com>
2025-07-07 22:11:34 -0400

    Make function for creating array of operators in order

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Global variables
+
 let a,o,b,ans,expDiv, lenNumAndOp;
 let exp = {
     a,o,b,
 }
-let equalToPressed = false;
 
+let lenExp;
 // Operator functions
 
 function add(a,b){
@@ -37,54 +39,98 @@ function operator(a,o,b){
     }
 }
 
+// Display Expression
+
 function displayExp(e){
-    console.log("displayExp is called")
     expDiv = document.getElementById("exp");
     let value = e.target.textContent;
     expDiv.textContent += value;
 }
 
-document.getElementById("equal").addEventListener("click", displayAns);
+// Events
+
+// document.getElementById("equal").addEventListener("click", displayAns);
+
+
+document.querySelectorAll(".equal").forEach(button=>{
+    button.addEventListener("click", () => {
+        displayAns();
+        arrSymFun();
+    })
+})
 
 document.querySelectorAll('.exp').forEach(button => {
     button.addEventListener('click', displayExp);
 });
+
+
+// document.querySelectorAll('.exp').forEach(button => {
+//     button.addEventListener('click', () => {
+//         displayExp;
+//         const indices = loopString();
+//         console.log(indices); // Or do something with the result
+//     });
+// });
+
+
 
 document.querySelectorAll('.operation').forEach(button => {
     button.addEventListener('click', firstNumAndOp);
 });
 
 
+// Calculate & Display Answer
 
 function displayAns(){
-    console.log("dispans is called")
     expDiv = document.getElementById("exp");
     let totalLength = expDiv.textContent.length;
-    console.log(totalLength);
-    console.log[(lenNumAndOp+1)];
-    console.log[totalLength-1];
-
     exp.b = +(expDiv.textContent.substring(lenNumAndOp,totalLength));
-    console.log(exp.a);
-    console.log(exp.o);
-    console.log(exp.b);
-
     ans = operator(exp.a, exp.o, exp.b); //Object properties go in as the input for this function
-
-    console.log(ans);
-
     const ansDiv = document.getElementById("ans");
     ansDiv.textContent = ans;
 }
 
 function firstNumAndOp(){
-    console.log("fnao is called")
     expDiv = document.getElementById("exp");
     lenNumAndOp = expDiv.textContent.length;
     exp.a = +(expDiv.textContent.substring(0,lenNumAndOp-1));
     exp.o = expDiv.textContent[lenNumAndOp-1];
-    console.log(exp.a);
-    console.log(exp.o);
+}
+
+// Logic
+
+// E.g: 1+1*2
+// Take the expression, loop through the string for /, *, +,- one by one. 
+// If / is encountered, then stop and evaluate that expression first
+// How to evaluate? -> loop until another sign or undefined is encountered towards the left. this is your first variable.
+// Loop until another sign is encountered or undefined. this is your second variable.
+// call operate on these two variables and the sign
+// replace that expression with the answer, then do the same again. in this case you get 1+2
+// then evaluate and finally once no more signs are remaining, return the answer.
+
+// Function to loop through string and get the array of operators in order of BODMAS
+
+function arrSymFun(){
+    expDiv = document.getElementById("exp");
+    lenExp = expDiv.textContent.length;
+
+    let arrSym = [];
+    
+    // First find all division/multiplication operators
+    for (let i = 0; i < lenExp; i++) {
+        if (expDiv.textContent[i] == '/' || expDiv.textContent[i] == '*') {
+            arrSym.push(i);
+        }
+    }
+
+    // Then addition/subtraction operators
+    for (let i = 0; i < lenExp; i++) {
+        if (expDiv.textContent[i] == '+' || expDiv.textContent[i] == '-') {
+            arrSym.push(i);
+        }
+    }
+    
+    return arrSym;
 }
 
 
